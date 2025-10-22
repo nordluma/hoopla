88867095ef13bd006618aea5b9a1e9405d2516cf
nordluma <maxnordlund.95@hotmail.com>
2025-10-22 20:37:43 +0300

    feat(cli/lib): filter stopword tokens

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,16 +1,17 @@
 import string
 
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies
+from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
 
 replament_table = str.maketrans("", "", string.punctuation)
 
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
+    stopwords = load_stopwords()
     matches = []
     for movie in movies:
-        query_tokens = tokenize_text(query)
-        title_tokens = tokenize_text(movie["title"])
+        query_tokens = remove_stopwords(tokenize_text(query), stopwords)
+        title_tokens = remove_stopwords(tokenize_text(movie["title"]), stopwords)
 
         if token_match(query_tokens, title_tokens) and movie not in matches:
             matches.append(movie)
@@ -26,6 +27,11 @@ def preprocess_text(text: str) -> str:
 
 def tokenize_text(text: str) -> list[str]:
     return list(filter(lambda t: t != "", preprocess_text(text).split()))
+
+
+def remove_stopwords(tokens: list[str], stopwords: list[str]) -> list[str]:
+    stopwords_set = set(w for w in stopwords)
+    return [t for t in tokens if t not in stopwords_set]
 
 
 def token_match(query_tokens: list[str], tokens: list[str]) -> bool:
