d0108dae9a75e8b65943c9f3dfca7f2899b1b699
nordluma <maxnordlund.95@hotmail.com>
2025-10-22 20:51:43 +0300

    feat(cli/lib): add stemming

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,5 +1,7 @@
 import string
 
+from nltk.stem import PorterStemmer
+
 from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
 
 replament_table = str.maketrans("", "", string.punctuation)
@@ -10,8 +12,8 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     stopwords = load_stopwords()
     matches = []
     for movie in movies:
-        query_tokens = remove_stopwords(tokenize_text(query), stopwords)
-        title_tokens = remove_stopwords(tokenize_text(movie["title"]), stopwords)
+        query_tokens = tokenize_text(query, stopwords)
+        title_tokens = tokenize_text(movie["title"], stopwords)
 
         if token_match(query_tokens, title_tokens) and movie not in matches:
             matches.append(movie)
@@ -25,13 +27,20 @@ def preprocess_text(text: str) -> str:
     return text.lower().translate(replament_table)
 
 
-def tokenize_text(text: str) -> list[str]:
-    return list(filter(lambda t: t != "", preprocess_text(text).split()))
+def tokenize_text(text: str, stopwords: list[str]) -> list[str]:
+    tokens = list(filter(lambda t: t != "", preprocess_text(text).split()))
+    filtered_tokens = remove_stopwords(tokens, stopwords)
+    return stem_tokens(filtered_tokens)
 
 
 def remove_stopwords(tokens: list[str], stopwords: list[str]) -> list[str]:
     stopwords_set = set(w for w in stopwords)
     return [t for t in tokens if t not in stopwords_set]
+
+
+def stem_tokens(tokens: list[str]) -> list[str]:
+    stemmer = PorterStemmer()
+    return list(map(lambda t: stemmer.stem(t), tokens))
 
 
 def token_match(query_tokens: list[str], tokens: list[str]) -> bool:
