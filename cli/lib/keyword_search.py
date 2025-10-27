f583756d792a703e64ae4d07a87d322ea1cb6b82
nordluma <maxnordlund.95@hotmail.com>
2025-10-27 18:41:38 +0200

    feat(cli): command to get bm25 tf for a term in a document

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -158,6 +158,12 @@ def bm25_idf_command(term: str) -> float:
     return idx.get_bm25_idf(term)
 
 
+def bm25_tf_command(doc_id: int, term: str, k1: float = BM25_K1) -> float:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_bm25_tf(doc_id, term, k1)
+
+
 def preprocess_text(text: str) -> str:
     return text.lower().translate(replament_table)
 
