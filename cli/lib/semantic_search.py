392df0e7f8ed8dbbd7779397f7b336b6b2d0db98
nordluma <maxnordlund.95@hotmail.com>
2025-10-28 23:50:10 +0200

    feat(cli/lib): load or generate movie embeddings

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,10 +1,50 @@
+import os
+import numpy as np
+
 from sentence_transformers import SentenceTransformer
 from torch import Tensor
+
+from .search_utils import CACHE_DIR, load_movies
+
+MOVIE_EMBEDDINGS_PATH = os.path.join(CACHE_DIR, "movie_embeddings.npy")
 
 
 class SemanticSearch:
     def __init__(self) -> None:
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+
+    def load_or_create_embeddings(self, documents: list[dict]):
+        self.documents = documents
+        for doc in self.documents:
+            self.document_map[doc["id"]] = doc
+
+        if os.path.exists(MOVIE_EMBEDDINGS_PATH):
+            self.embeddings = np.load(MOVIE_EMBEDDINGS_PATH)
+            if len(self.embeddings) == len(self.documents):
+                return self.embeddings
+
+        return self.build_embeddings(documents)
+
+    def build_embeddings(self, documents: list[dict]):
+        self.documents = documents
+
+        doc_list = []
+        for doc in self.documents:
+            self.document_map[doc["id"]] = doc
+            doc_list.append(f"{doc['title']}: {doc['description']}")
+
+        self.embeddings = self.model.encode(
+            sentences=doc_list,
+            show_progress_bar=True,
+        )
+
+        os.makedirs(MOVIE_EMBEDDINGS_PATH, exist_ok=True)
+        np.save(MOVIE_EMBEDDINGS_PATH, self.embeddings)
+
+        return self.embeddings
 
     def generate_embedding(self, text: str) -> Tensor:
         if text.strip() == "":
